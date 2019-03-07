e1916e7c91ab7d5128fe3d63114e4c5d3a8d146b
Nicolas <nmurgui@gmail.com>
2019-03-07 11:38:43 +0100

    Handling buggy file

---
+++ Software/Development/SemesterProject/App/systemManager.c
@@ -0,0 +1,70 @@
+//###########################################################################
+//FILE: systemManager.c
+//
+//Project: Electric Go-kart
+//
+//Author: Nicolas Murguizur, 3/3/19
+//
+//Notes:
+//      System's state FSM is included in this file. More details about
+//      might be found in the detailed class description:
+//      SemesterProjectPED2\Software\Design\SystemArchitectureDescription.pdf
+//
+//###########################################################################
+
+//###########################################################################
+
+//
+// Included Files
+//
+
+#include "Include/systemManager.h"
+#include "Include/digitalInputManager.h"
+
+
+//
+// Quasi-global variables definition
+//
+
+static SysMgrState SystemState = STARTUP;
+
+
+void manageSystem(void)
+{
+
+    switch (SystemState)
+    {
+        case STARTUP:
+        {
+            if(startupSequenceFinished()) SystemState = STANDBY;
+        }break;
+
+        case STANDBY:
+        {
+            if(isPowerSwitchEnabled()) SystemState = RUNNING;
+        }break;
+
+        case RUNNING:
+        {
+            if(!isPowerSwitchEnabled()) SystemState = STANDBY;
+           // if(errorDetected()) SystemState = ERROR;
+        }break;
+
+//        case ERROR:
+//        {
+//            if(errorIsAcknowledged()) SystemState = STANDBY;
+//        }break;
+    }
+
+}
+
+SysMgrState readSystemState(void)
+{
+    return SystemState;
+}
+
+
+//
+// End of File
+//
+
