2193c47b2dfec6754523263eaa611f04a7627c86
Nicolas <nmurgui@gmail.com>
2019-03-07 11:21:39 +0100

    FML buggy file

--- Software/Development/SemesterProject/App/systemManager.c
+++ Software/Development/SemesterProject/App/systemManager.c
@@ -1,70 +1,22 @@
 //###########################################################################
-//FILE: systemManager.c
+//FILE: main.c
 //
 //Project: Electric Go-kart
 //
-//Author: Nicolas Murguizur, 3/3/19
+//Author: PED-841 AAU, February 2019 - May 2019.
 //
 //Notes:
-//      System's state FSM is included in this file. More details about
-//      might be found in the detailed class description:
-//      SemesterProjectPED2\Software\Design\SystemArchitectureDescription.pdf
+//    The system initialization has been performed using TI's blinking LED example.
+//    TI Data:  $TI Release: F2806x Support Library v2.04.00.00 $
+//              $Release Date: Thu Oct 18 15:47:20 CDT 2018 $
+//
+//!  This example configures CPU Timer0 for a 500 msec period, and toggles the 
+//!  GPIO34 LED once per interrupt. For testing purposes, this example
+//!  also increments a counter each time the timer asserts an interrupt.
+//!
+//!  \b Watch \b Variables \n
+//!  - CpuTimer0.InterruptCount
+//!
 //
 //###########################################################################
-
-//###########################################################################
-
-//
-// Included Files
-//
-
-#include "Include/systemManager.h"
-#include "Include/digitalInputManager.h"
-
-
-//
-// Quasi-global variables definition
-//
-
-static SysMgrState SystemState = STARTUP;
-
-
-void manageSystem(void)
-{
-
-    switch (SystemState)
-    {
-        case STARTUP:
-        {
-            if(startupSequenceFinished()) SystemState = STANDBY;
-        }break;
-
-        case STANDBY:
-        {
-            if(isPowerSwitchEnabled()) SystemState = RUNNING;
-        }break;
-
-        case RUNNING:
-        {
-            if(!isPowerSwitchEnabled()) SystemState = STANDBY;
-           // if(errorDetected()) SystemState = ERROR;
-        }break;
-
-//        case ERROR:
-//        {
-//            if(errorIsAcknowledged()) SystemState = STANDBY;
-//        }break;
-    }
-
-}
-
-SysMgrState readSystemState(void)
-{
-    return SystemState;
-}
-
-
-//
-// End of File
-//
 
