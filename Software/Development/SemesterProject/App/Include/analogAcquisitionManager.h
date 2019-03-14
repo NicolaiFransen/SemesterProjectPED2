22fdaa4e757b6b56c402b3d4647b832e7927ce9d
NicolaiFransen <fransendk@gmail.com>
2019-03-14 21:28:49 +0100

    Creation of signal list and define of ADC channels

--- Software/Development/SemesterProject/App/Include/analogAcquisitionManager.h
+++ Software/Development/SemesterProject/App/Include/analogAcquisitionManager.h
@@ -13,11 +13,34 @@
 //
 #include "DSP28x_Project.h"     // Device Headerfile and Examples Include File
 #include "analogSignal.h"
+
+//
+// ADC Channel definitions
+//
+#define P4      0       // ADCINA0
+#define P2      1       // ADCINA1
+#define J4      2       // ADCINA2
+#define IA      3       // ADCINA3
+// #define       4       // ADCINA4   -- FREE PIN
+#define IC      5       // ADCINA5
+#define J1      6       // ADCINA6
+#define J2      7       // ADCINA7
+#define P5      8       // ADCINB0
+#define J3      9       // ADCINB1
+#define P3      10      // ADCINB2
+// #define       11      // ADCINB3   -- FREE PIN
+#define AD36    12      // ADCINB4
+#define IB      13      // ADCINB5
+#define AD24    14      // ADCINB6
+#define P1      15      // ADCINB7
+
 //
 // Function prototyping
 //
 void readAnalogSignals(void);
+void calculateFilteredValue(void);
 void initADC(void);
+void configureAnalogSignals(void);
 
 
 
--- Software/Development/SemesterProject/App/analogAcquisitionManager.c
+++ Software/Development/SemesterProject/App/analogAcquisitionManager.c
@@ -23,57 +23,124 @@
 //
 // Quasi-global variables definition
 //
-// Initialize all Analog signals
 static struct
 {
-    AnalogSignal sig1;
-    AnalogSignal sig2;
+    AnalogSignal currentMeasA;
+    AnalogSignal currentMeasB;
+    AnalogSignal currentMeasC;
+//    AnalogSignal voltageMeas24;
+//    AnalogSignal voltageMeas36;
+//    AnalogSignal thermalMeas1;
+//    AnalogSignal thermalMeas2;
+//    AnalogSignal sliderPotLeft;
+//    AnalogSignal sliderPotRight;
+//    AnalogSignal rottaryPot1;
+//    AnalogSignal rottaryPot2;
+//    AnalogSignal rottaryPot3;
+//    AnalogSignal connectorPot1;
+//    AnalogSignal connectorPot2;
 } AnalogSignalList;
+// Initialize all Analog signals
+
 
 void initADC(void)
 {
-    char type = 'L';
-    int order = 1, filterFreq = 50;
-    Uint16 sig1Channel = 0, sig2Channel = 1;
-    int threshold[2] = {300, 3000};
-    Signal_Constructor(&AnalogSignalList.sig1, type, order, filterFreq, sig1Channel, threshold);
-    Signal_Constructor(&AnalogSignalList.sig2, type, order, filterFreq, sig2Channel, threshold);
-
     InitAdc();
-    // AdcOffsetSelfCal();
+    AdcOffsetSelfCal();
+
+    configureAnalogSignals();
+
+    PieCtrlRegs.PIEIER1.bit.INTx1 = 1;
+    IER |= M_INT1;                     // Enable CPU Interrupt 1
 
     EALLOW;
+    // Enable non-overlap mode
     AdcRegs.ADCCTL2.bit.ADCNONOVERLAP = 1;
+
+    // ADCINT1 trips after AdcResults latch
     AdcRegs.ADCCTL1.bit.INTPULSEPOS = 1;
+    AdcRegs.INTSEL1N2.bit.INT1E = 1;
+    AdcRegs.INTSEL1N2.bit.INT1CONT = 0;
+
+    // setup EOC1 to trigger ADCINT1 to fire
+    AdcRegs.INTSEL1N2.bit.INT1SEL = 1;
 
     // Setting all ADCs to sequential sampling
     AdcRegs.ADCSAMPLEMODE.bit.SIMULEN0 = 0;
 
-    AdcRegs.ADCSOC0CTL.bit.CHSEL = AnalogSignalList.sig1.adcChannel;
-    AdcRegs.ADCSOC1CTL.bit.CHSEL = AnalogSignalList.sig2.adcChannel;
-
-    AdcRegs.ADCSOC0CTL.bit.TRIGSEL = 1;
-    AdcRegs.ADCSOC1CTL.bit.TRIGSEL = 1;
-
-    AdcRegs.ADCSOC0CTL.bit.ACQPS = 6;
-    AdcRegs.ADCSOC1CTL.bit.ACQPS = 25;
-
-    AdcRegs.INTSEL1N2.bit.INT1SEL = 1;
-    AdcRegs.INTSEL1N2.bit.INT1E = 1;
-    AdcRegs.INTSEL1N2.bit.INT1CONT = 0;
-
     EDIS;
 
+    //
+    // Assumes ePWM1 clock is already enabled in InitSysCtrl();
+    //
+    EPwm1Regs.ETSEL.bit.SOCAEN  = 1;        // Enable SOC on A group
+    EPwm1Regs.ETSEL.bit.SOCASEL = 4;        // Select SOC from CMPA on upcount
+    EPwm1Regs.ETPS.bit.SOCAPRD  = 1;        // Generate pulse on 1st event
+    EPwm1Regs.CMPA.half.CMPA    = 0x0080;   // Set compare A value
+    EPwm1Regs.TBPRD             = 0xFFFF;   // Set period for ePWM1
+    EPwm1Regs.TBCTL.bit.CTRMODE = 0;        // count up and start
+}
 
+/*
+ * All the analog signals are created and linked to the ADC channels here.
+ */
+void configureAnalogSignals(void)
+{
+    char filterType = 'L';
+    int filterOrder = 1, filterFreq = 580;
 
+    // Configure of Channel ADCINA3. Current A measurement.
+    Uint16 currentMeasAChannel = IA;
+    int currentThreshold[2] = {300, 3000};
+    Signal_Constructor(&AnalogSignalList.currentMeasA, filterType, filterOrder,
+                       filterFreq, currentMeasAChannel, currentThreshold);
+
+    AdcRegs.ADCSOC3CTL.bit.CHSEL = AnalogSignalList.currentMeasA.adcChannel;
+    AdcRegs.ADCSOC3CTL.bit.TRIGSEL = 5;
+    AdcRegs.ADCSOC3CTL.bit.ACQPS = 26;
+
+    // Configure of Channel ADCINB5. Current B measurement.
+    Uint16 currentMeasBChannel = IB;
+    Signal_Constructor(&AnalogSignalList.currentMeasB, filterType, filterOrder,
+                       filterFreq, currentMeasBChannel, currentThreshold);
+
+    AdcRegs.ADCSOC13CTL.bit.CHSEL = AnalogSignalList.currentMeasB.adcChannel;
+    AdcRegs.ADCSOC13CTL.bit.TRIGSEL = 5;
+    AdcRegs.ADCSOC13CTL.bit.ACQPS = 26;
+
+    // Configure of Channel ADCINA5. Current C measurement.
+    Uint16 currentMeasCChannel = IC;
+    Signal_Constructor(&AnalogSignalList.currentMeasC, filterType, filterOrder,
+                       filterFreq, currentMeasCChannel, currentThreshold);
+
+    AdcRegs.ADCSOC5CTL.bit.CHSEL = AnalogSignalList.currentMeasC.adcChannel;
+    AdcRegs.ADCSOC5CTL.bit.TRIGSEL = 5;
+    AdcRegs.ADCSOC5CTL.bit.ACQPS = 26;
 }
 
 void readAnalogSignals(void)
 {
-    Uint16 valueSig1 = readADCValue(&AnalogSignalList.sig1);
-    Uint16 valueSig2 = readADCValue(&AnalogSignalList.sig2);
+    AnalogSignal *structPointer;
+    AnalogSignal *initialMemoryPosition = &AnalogSignalList.currentMeasA;
+    AnalogSignal *finalMemoryPosition = initialMemoryPosition + sizeof(AnalogSignalList);
+
+    for (structPointer = initialMemoryPosition; structPointer < finalMemoryPosition; structPointer++)
+        readADCValue(structPointer);
+
+    //    AnalogSignalList.currentMeasA.lastObtainedValue = readADCValue(&AnalogSignalList.currentMeasA);
+//    AnalogSignalList.currentMeasB.lastObtainedValue = readADCValue(&AnalogSignalList.currentMeasB);
+//    AnalogSignalList.currentMeasC.lastObtainedValue = readADCValue(&AnalogSignalList.currentMeasC);
 }
 
+void calculateFilteredValue(void)
+{
+    AnalogSignal *structPointer;
+    AnalogSignal *initialMemoryPosition = &AnalogSignalList.currentMeasA;
+    AnalogSignal *finalMemoryPosition = initialMemoryPosition + sizeof(AnalogSignalList);
+
+    for (structPointer = initialMemoryPosition; structPointer < finalMemoryPosition; structPointer++)
+        filterADCValue(structPointer);
+}
 /*
 // initialization of all digital inputs {pin, state}
 static struct
