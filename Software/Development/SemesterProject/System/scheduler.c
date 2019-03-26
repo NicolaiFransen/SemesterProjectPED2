61d4f94072ae5ed643b0e57cea1d7a584eac0cec
AitorTeran <35167655+AitorTeran@users.noreply.github.com>
2019-03-26 15:31:00 +0100

    openLoop final push, ready for merging.

--- Software/Development/SemesterProject/System/scheduler.c
+++ Software/Development/SemesterProject/System/scheduler.c
@@ -76,7 +76,7 @@ void runTask(void (*functionPTR)())
  */
 void task100us(void)
 {//running at switching 10kHz (update with switching frequency);
-    openLoopVFControl();
+    return;
 }
 void task50us(void)
 {
