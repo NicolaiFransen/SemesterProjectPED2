71702a817ab6dd804a51707e0e616fe8a03e103f
NicolaiFransen <fransendk@gmail.com>
2019-04-10 09:39:55 +0200

    Controllers for current and torque loop created

--- Software/Development/SemesterProject/App/Include/PIController.h
+++ Software/Development/SemesterProject/App/Include/PIController.h
@@ -13,10 +13,36 @@ typedef struct PIobjectTag
 {
     int KP;
     int KI;
+    float integralPart;
+    float referencePoint;
+    float previousOutput;
+    float previousLimitedOutput;
+    float saturationLimit;
 } PIobject;
 
-void PIObject_Constructor(PIobject *PI, int KP, int KI);
+/*
+ * Initialization functions
+ */
+void PIObject_Constructor(PIobject *PI, int KP, int KI, float saturationLimit);
 void initPIControllers(void);
-float PiCalculation(PIobject *PI, int errors[]);
+
+/*
+ * PI controllers
+ */
+float PiCurrentCalculation(PIobject *PI, float measurementValue);
+float PiTorqueCalculation(PIobject *PI, float measurementValue);
+
+/*
+ * Function to check saturation of output
+ */
+int isOutputSaturatedPositive(PIobject *PI);
+int isOutputSaturatedNegative(PIobject *PI);
+
+/*
+ * Functions to change reference point
+ */
+void setIqReferencePoint(float referencePoint);
+void setIdReferencePoint(float referencePoint);
+void setTorqueReferencePoint(float referencePoint);
 
 #endif /* APP_INCLUDE_PICONTROLLER_H_ */
--- Software/Development/SemesterProject/App/PIController.c
+++ Software/Development/SemesterProject/App/PIController.c
@@ -3,6 +3,12 @@
  *
  *  Created on: 2. apr. 2019
  *      Author: Nicolai Fransen
+ *
+ *  This file includes the PI controllers for the control system.
+ *  Two different PI functions have been created because the outer loop
+ *  must include anti-windup, such that the integrator will not get stuck.
+ *
+ *  An if-statement is included to check if the output should be limited.
  */
 
 //
@@ -14,8 +20,6 @@
 //
 // Quasi-global variables definition
 //
-
-
 static struct
 {
     PIobject IqController;
@@ -23,30 +27,121 @@ static struct
     PIobject TorqueController;
 } PIControllerList;
 
-void PIObject_Constructor(PIobject *PIcontroller, int KP, int KI)
+
+void PIObject_Constructor(PIobject *PIcontroller, int KP, int KI, float saturationThreshold)
 {
     PIcontroller->KP = KP;
     PIcontroller->KI = KI;
+    PIcontroller->saturationLimit = saturationThreshold;
+
+    PIcontroller->integralPart = 0;
+    PIcontroller->previousOutput = 0;
+    PIcontroller->previousLimitedOutput = 0;
 }
 
 void initPIControllers(void)
 {
-    PIObject_Constructor(&PIControllerList.IdController, 10, 10);
-    PIObject_Constructor(&PIControllerList.IdController, 10, 10);
-    PIObject_Constructor(&PIControllerList.TorqueController, 10, 10);
+    PIObject_Constructor(&PIControllerList.IdController, 10, 10, 10);
+    PIObject_Constructor(&PIControllerList.IdController, 10, 10, 10);
+    PIObject_Constructor(&PIControllerList.TorqueController, 10, 10, 10);
 }
 
-float PiCalculation(PIobject *PIcontroller, int errors[])
+/*
+ * PI controller for the inner loops.
+ */
+float PiCurrentCalculation(PIobject *PIcontroller, float measureredValue)
 {
     int KP = PIcontroller->KP;
     int KI = PIcontroller->KI;
-    int arrayPosition;
-    float output = 0, sumOfErrors = 0;
 
-    for (arrayPosition = 0; arrayPosition < sizeof(errors); arrayPosition++)
-        sumOfErrors = sumOfErrors + errors[arrayPosition];
+    float error, PiOutput;
 
-    output = KP*errors[0] + KI*sumOfErrors;
+    // Calculating the difference between the reference and the measured
+    error = PIcontroller->referencePoint - measureredValue;
 
-    return output;
+    // Calculating the integral part from the error
+    PIcontroller->integralPart = PIcontroller->integralPart + error;
+
+    // Calculating the controller output
+    PiOutput = KP * error + KI * PIcontroller->integralPart;
+
+    // If the wanted output is outside saturation limits, then limit the output
+    if (isOutputSaturated(PIcontroller))
+        PiOutput = PIcontroller->saturationLimit;
+
+    else if (isOutputSaturatedNegative(PIcontroller))
+        PiOutput = -(PIcontroller->saturationLimit);
+
+    return PiOutput;
 }
+
+/*
+ * PI controller for outer loop.
+ * This controller includes an anti-windup feedback for the integrator
+ */
+float PiTorqueCalculation(PIobject *PIcontroller, float measureredValue)
+{
+    int KP = PIcontroller->KP;
+    int KI = PIcontroller->KI;
+
+    float error, PiOutput, outputFeedback;
+
+    // Calculating the difference between the reference and the measured
+    error = PIcontroller->referencePoint - measureredValue;
+
+    // Calculating the integral part from the error
+    PIcontroller->integralPart = PIcontroller->integralPart + error;
+
+    // Calculating the difference between the wanted output and the actual output
+    // To be used in the anti-windup of the integrator
+    outputFeedback = PIcontroller->previousOutput - PIcontroller->previousLimitedOutput;
+
+    // Calculating the controller output
+    PiOutput = KP * error +
+              (KI * PIcontroller->integralPart - outputFeedback * 1/KP);
+
+    PIcontroller->previousOutput = PiOutput;
+
+    // If the wanted output is outside saturation limits, then limit the output
+    if (isOutputSaturatedPositive(PIcontroller))
+        PiOutput = PIcontroller->saturationLimit;
+
+    else if (isOutputSaturatedNegative(PIcontroller))
+        PiOutput = -(PIcontroller->saturationLimit);
+
+    PIcontroller->previousLimitedOutput = PiOutput;
+
+    return PiOutput;
+}
+
+/*
+ * Functions to check for saturation
+ */
+int isOutputSaturatedPositive(PIobject *PIcontroller)
+{
+    return PIcontroller->previousOutput > PIcontroller->saturationLimit;
+}
+
+int isOutputSaturatedNegative(PIobject * PIcontroller)
+{
+    return PIcontroller->previousOutput < -(PIcontroller->saturationLimit);
+}
+
+/*
+ * Functions to set reference points for each PI controller
+ */
+void setIqReferencePoint(float referencePoint)
+{
+    PIControllerList.IqController.referencePoint = referencePoint;
+}
+
+void setIdReferencePoint(float referencePoint)
+{
+    PIControllerList.IdController.referencePoint = referencePoint;
+}
+
+void setTorqueReferencePoint(float referencePoint)
+{
+    PIControllerList.TorqueController.referencePoint = referencePoint;
+}
+
