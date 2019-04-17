4f55662ac96c1f68bc87f8d8f48eae14ef68c85c
AitorTeran <35167655+AitorTeran@users.noreply.github.com>
2019-04-17 19:27:33 +0200

    Slip angle created

---
+++ Software/Development/SemesterProject/App/Include/SlipAngleCalculator.h
@@ -0,0 +1,19 @@
+/*
+ * SlipAngleCalculator.h
+ *
+ *  Created on: 17 abr. 2019
+ *      Author: aitor
+ */
+
+#ifndef APP_INCLUDE_SLIPANGLECALCULATOR_H_
+#define APP_INCLUDE_SLIPANGLECALCULATOR_H_
+
+
+typedef struct
+{
+    float posRad;
+    float speedHz;
+} slipAngle;
+
+
+#endif /* APP_INCLUDE_SLIPANGLECALCULATOR_H_ */
---
+++ Software/Development/SemesterProject/App/SlipAngleCalculator.c
@@ -0,0 +1,13 @@
+/*
+ * SlipAngleCalculator.c
+ *
+ *  Created on: 17 abr. 2019
+ *      Author: Aitor Teran
+ */
+
+#include "SlipAngleCalculator.h"
+
+slipAngle slipAngleObject;
+
+
+
