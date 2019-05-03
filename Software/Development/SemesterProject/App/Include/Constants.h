d7b29c2aeb8479c1275151e636397993d6280029
NicolaiFransen <fransendk@gmail.com>
2019-05-03 09:02:28 +0200

    Tried to fix Aitors OCD

--- Software/Development/SemesterProject/App/Include/Constants.h
+++ Software/Development/SemesterProject/App/Include/Constants.h
@@ -10,54 +10,54 @@
 //Processor related parameters
 #define CLOCK_FREQUENCY             ((float)150e6)          // 150 MHz clock frequency
 #define CLOCK_PERIOD                ((float)6.667e-9)       // 1/CLOCK_FREQUENCY clock period
-#define SW_FREQ                             10000           //Switching frequency [10kHz].
-#define SW_PERIOD_US                        100             //Period of the PWM.
+#define SW_FREQ                     10000                   //Switching frequency [10kHz].
+#define SW_PERIOD_US                100                     //Period of the PWM.
 #define SW_PERIOD_S                 ((float)100e-6)         //PWM period in s.
-#define INTERNAL_FREQ                       90000000        //Internal frequency of the device [90MHz]. Configured in InitSysCtrl().
+#define INTERNAL_FREQ               90000000                //Internal frequency of the device [90MHz]. Configured in InitSysCtrl().
 
 #define MinPulseWidth	            ((float)2e-6)           //minimum pulse width in s
 #define MinPulseWidthCNT	        ((unsigned short)300)   //MinPulseWidth/CLOCK_PERIOD (2us)
 #define Half_MinPulseWidthCNT	    ((unsigned short)150)	//half MinPulseWidth/CLOCK_PERIOD (1us)
 
 //Mathematical Constants
-#define PI      		((float)(3.141592653589793))        // pi
-#define TWO_PI      	((float)(6.283185307179586))        // 2*pi
-#define THREE_PI_DIV_2	((float)(4.712388980384690))        // '3*pi/2'
-#define PI_DIV_2 		((float)(1.570796326794897))        // 'pi/2'
-#define PI_DIV_6		((float)(0.523599))                 // 'pi/6'
-#define PI_DIV_3		((float)(1.047198))                 // 'pi/3'
-#define TWO_PI_DIV_3	((float)(2.094395))                 // '2*pi/3'
-#define FOUR_PI_DIV_3	((float)(4.18879))                  // '4*pi/3'
-#define FIVE_PI_DIV_3	((float)(5.235988))                 // '5*pi/3'
+#define PI      		    ((float)(3.141592653589793))    // pi
+#define TWO_PI      	    ((float)(6.283185307179586))    // 2*pi
+#define THREE_PI_DIV_2	    ((float)(4.712388980384690))    // '3*pi/2'
+#define PI_DIV_2 		    ((float)(1.570796326794897))    // 'pi/2'
+#define PI_DIV_6		    ((float)(0.523599))             // 'pi/6'
+#define PI_DIV_3		    ((float)(1.047198))             // 'pi/3'
+#define TWO_PI_DIV_3	    ((float)(2.094395))             // '2*pi/3'
+#define FOUR_PI_DIV_3	    ((float)(4.18879))              // '4*pi/3'
+#define FIVE_PI_DIV_3	    ((float)(5.235988))             // '5*pi/3'
 
-#define  SQRT_2         ((float)1.41421356237309)           // sqrt(2)
-#define  SQRT_3         ((float)1.73205080756887)           // sqrt(3)
+#define  SQRT_2             ((float)1.41421356237309)       // sqrt(2)
+#define  SQRT_3             ((float)1.73205080756887)       // sqrt(3)
 
 // set implementation definitions:
-#define ONE_DIV_3       ((float)0.333333333333333)          // '1/3'
-#define TWO_DIV_3       ((float)0.666666666666667)          // '2/3'
-#define SQRT2_DIV_3     ((float)0.471404520791032)          // 'sqrt(2)/3'
-#define SQRT3_DIV_2     ((float)0.866025403784439)          // 'sqrt(3)/2'
-#define SQRT3		    ((float)1.732050807568877)          // 'sqrt(3)/2'
-#define ONE_DIV_SQRT2   ((float)0.707106781186547)          // '1/sqrt(2)'
-#define ONE_DIV_SQRT3   ((float)0.577350269189626)          // '1/sqrt(3)'
-#define ONE_DIV_SQRT6   ((float)0.408248290463863)          // '1/sqrt(6)'
-#define TWO_DIV_SQRT6   ((float)0.816496580927726)          // '2/sqrt(6)'
-#define TWO_DIV_SQRT3   ((float)1.154700538379252)          // '2/sqrt(3)'
-#define TWO_SQRT3_DIV_PI ((float)1.102657790843584)         // '2*sqrt(3)/pi'
+#define ONE_DIV_3           ((float)0.333333333333333)      // '1/3'
+#define TWO_DIV_3           ((float)0.666666666666667)      // '2/3'
+#define SQRT2_DIV_3         ((float)0.471404520791032)      // 'sqrt(2)/3'
+#define SQRT3_DIV_2         ((float)0.866025403784439)      // 'sqrt(3)/2'
+#define SQRT3		        ((float)1.732050807568877)      // 'sqrt(3)/2'
+#define ONE_DIV_SQRT2       ((float)0.707106781186547)      // '1/sqrt(2)'
+#define ONE_DIV_SQRT3       ((float)0.577350269189626)      // '1/sqrt(3)'
+#define ONE_DIV_SQRT6       ((float)0.408248290463863)      // '1/sqrt(6)'
+#define TWO_DIV_SQRT6       ((float)0.816496580927726)      // '2/sqrt(6)'
+#define TWO_DIV_SQRT3       ((float)1.154700538379252)      // '2/sqrt(3)'
+#define TWO_SQRT3_DIV_PI    ((float)1.102657790843584)      // '2*sqrt(3)/pi'
 
 // definitions for sector angle limits
-#define DEG_10_TO_RAD   ((float)0.174532925199433)          // '10 deg --> rad'
-#define DEG_30_TO_RAD   ((float)0.523598775598299)          // '30 deg --> rad'
-#define DEG_60_TO_RAD   ((float)1.047197551196598)          // '60 deg --> rad'
-#define DEG_90_TO_RAD   ((float)1.570796326794897)          // '90 deg --> rad'
-#define DEG_120_TO_RAD  ((float)2.094395102393195)          // '120 deg --> rad'
-#define DEG_180_TO_RAD  ((float)3.141592653589793)          // '180 deg --> rad'
-#define DEG_240_TO_RAD  ((float)4.188790204786391)          // '240 deg --> rad'
-#define DEG_300_TO_RAD  ((float)5.235987755982989)          // '300 deg --> rad'
-#define DEG_360_TO_RAD  ((float)6.283185307179586)          // '360 deg --> rad'
-#define REV_TO_RAD      ((float)6.283185307179586)          // '1 rev   --> rad'
-#define RAD_TO_REV      ((float)0.159154943091895)          // '1 rad   --> rev'
+#define DEG_10_TO_RAD       ((float)0.174532925199433)      // '10 deg --> rad'
+#define DEG_30_TO_RAD       ((float)0.523598775598299)      // '30 deg --> rad'
+#define DEG_60_TO_RAD       ((float)1.047197551196598)      // '60 deg --> rad'
+#define DEG_90_TO_RAD       ((float)1.570796326794897)      // '90 deg --> rad'
+#define DEG_120_TO_RAD      ((float)2.094395102393195)      // '120 deg --> rad'
+#define DEG_180_TO_RAD      ((float)3.141592653589793)      // '180 deg --> rad'
+#define DEG_240_TO_RAD      ((float)4.188790204786391)      // '240 deg --> rad'
+#define DEG_300_TO_RAD      ((float)5.235987755982989)      // '300 deg --> rad'
+#define DEG_360_TO_RAD      ((float)6.283185307179586)      // '360 deg --> rad'
+#define REV_TO_RAD          ((float)6.283185307179586)      // '1 rev   --> rad'
+#define RAD_TO_REV          ((float)0.159154943091895)      // '1 rad   --> rev'
 
 
 // definitions specific to the motor
@@ -72,13 +72,13 @@
 #define ID_RATED            ((float)149.2287)               // 'Id rated current --> A'
 #define ID_RATED_INVERSE    ((float)0.006701123845)         // 'Id rated current inverse --> A^-1'
 
-#define ENCODER_STEPS          2048
+#define ENCODER_STEPS         2048
 #define ENCODER_STEPS_INVERSE ((float)(1.0 / ENCODER_STEPS))
-#define POLE_PAIRS             2                            //Number of pole pairs of the motor.
+#define POLE_PAIRS            2                             //Number of pole pairs of the motor.
 #define POLE_PAIRS_INVERSE    ((float)0.5)                  //To be used instead of a division.
 
-#define TORQUE_TO_Q_CURRENT     1/(TWO_DIV_3 * POLE_PAIRS * (LM/LR) * LAMDA_R)
-#define D_CURRENT_REFERENCE     LAMDA_R/LM
+#define TORQUE_TO_Q_CURRENT   1/(TWO_DIV_3 * POLE_PAIRS * (LM/LR) * LAMDA_R)
+#define D_CURRENT_REFERENCE   LAMDA_R/LM
 
 #define d_axis			0
 #define	q_axis			1
